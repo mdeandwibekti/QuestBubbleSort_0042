c388a3a0641c0b91463656f05c891beca22c48ff
mdeandwibekti <mdeandwibekti@gmail.com>
2025-03-11 11:48:00 +0700

    membuat void display

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -43,5 +43,25 @@ void bubbleshortarray() {
 
 		}
 		cout << endl;
-	} while (pass <= n - 1);   //step 5
+	} while (pass <= n - 1); 
 }
+
+void display() {
+	cout << endl;
+	cout << "=================" << endl;
+	cout << "element array yang sudah tersusun" << endl;
+	cout << "=================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << " --> ";
+		}
+	}
+	cout << endl;
+	cout << endl;
+	cout << "jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
+
+//m
